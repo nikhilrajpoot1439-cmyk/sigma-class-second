ec4cc2f06c57ad184ba24581ab0fa8e8bdc0c20c
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-03-25 21:00:26 +0545

    javascript

---
+++ video 54/script.js
@@ -0,0 +1,17 @@
+alert("hello nikhil");
+console.log("Hey i am 2 nikhil")
+// console.log("Hey i am 3 nikhil")
+// console.log("Hey i am 4 nikhil")
+console.log("Hey i am 5 nikhil")
+var a =prompt("Enter your Name")
+console.log("your name is " +a)
+
+var istrue = confirm("Are you sure you want to leave our site and blast your computer")
+
+if(istrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+document.body.style.backgroundcolor = "Red"
