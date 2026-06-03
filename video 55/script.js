4aadcd4c3ca2908d316cafd72ff74da14f6acbaf
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-03 22:08:24 +0545

    sigma

---
+++ video 55/script.js
@@ -0,0 +1,9 @@
+console.log("Nikhil Rajput");
+let a = 6;
+let b=7;
+let c=9;
+// var d=niyty;
+console.log(a*b+c)
+console.log(typeof a, typeof b, typeof c)
+
+// var is always start with letter , symbol not number --
