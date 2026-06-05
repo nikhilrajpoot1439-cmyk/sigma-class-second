22f52304d21221cd7e229b89c8a4d70710d717d2
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-05 23:09:02 +0545

    web dev

---
+++ video 54 b/script.js
@@ -0,0 +1,14 @@
+alert("Hello World.....!");
+
+console.log("My Name is Nikhil")
+var a = prompt("Enter your Number")
+
+var isTrue = prompt("Are You sure you want to leave our site and blast you PC")
+
+if (isTrue) {
+    console.log("Computer is blasting......!");
+}
+else {
+    console.log("Computer is not blasting");
+}
+console.log("Your number is " + a)
---
+++ video 54/script1.js
@@ -0,0 +1,5 @@
+alert("Hello World.....!");
+
+console.log("My Name is Nikhil")
+var a = promp
+
